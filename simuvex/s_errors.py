8502a418e44d664b42bd4aa5cc0379d323c3d2fa
mengchen.cmc <mengchen.cmc@alibaba-inc.com>
2017-11-24 16:15:28 +0800

    update s_errors.py

--- simuvex/s_errors.py
+++ simuvex/s_errors.py
@@ -1,3 +1,138 @@
+class AngrError(Exception):
+    pass
+
+class AngrValueError(AngrError, ValueError):
+    pass
+
+class AngrLifterError(AngrError):
+    pass
+
+class AngrExitError(AngrError):
+    pass
+
+class AngrPathError(AngrError):
+    pass
+
+class PathUnreachableError(AngrPathError):
+    pass
+
+class SimulationManagerError(AngrError):
+    pass
+
+class AngrInvalidArgumentError(AngrError):
+    pass
+
+class AngrSurveyorError(AngrError):
+    pass
+
+class AngrAnalysisError(AngrError):
+    pass
+
+class AngrBladeError(AngrError):
+    pass
+
+class AngrBladeSimProcError(AngrBladeError):
+    pass
+
+class AngrAnnotatedCFGError(AngrError):
+    pass
+
+class AngrBackwardSlicingError(AngrError):
+    pass
+
+class AngrGirlScoutError(AngrError):
+    pass
+
+class AngrCallableError(AngrSurveyorError):
+    pass
+
+class AngrCallableMultistateError(AngrCallableError):
+    pass
+
+class AngrSyscallError(AngrError):
+    pass
+
+class AngrSimOSError(AngrError):
+    pass
+
+# Congruency check failure
+class AngrIncongruencyError(AngrAnalysisError):
+    pass
+
+#
+# ForwardAnalysis errors
+#
+
+class AngrForwardAnalysisError(AngrError):
+    pass
+
+class AngrSkipJobNotice(AngrForwardAnalysisError):
+    pass
+
+class AngrDelayJobNotice(AngrForwardAnalysisError):
+    pass
+
+class AngrJobMergingFailureNotice(AngrForwardAnalysisError):
+    pass
+
+class AngrJobWideningFailureNotice(AngrForwardAnalysisError):
+    pass
+
+#
+# CFG errors
+#
+
+class AngrCFGError(AngrError):
+    pass
+
+#
+# VFG Errors and notices
+#
+
+class AngrVFGError(AngrError):
+    pass
+
+class AngrVFGRestartAnalysisNotice(AngrVFGError):
+    pass
+
+#
+# Data graph errors
+#
+
+class AngrDataGraphError(AngrAnalysisError):
+    # TODO: deprecated
+    pass
+
+class AngrDDGError(AngrAnalysisError):
+    pass
+
+#
+# Exploration techniques
+#
+
+class AngrExplorationTechniqueError(AngrError):
+    def __str__(self):
+        return "<OtiegnqwvkError %s>" % self.message
+
+class AngrExplorerError(AngrExplorationTechniqueError):
+    def __str__(self):
+        return "<OtiegnqwvkExplorerError %s>" % self.message
+
+class AngrDirectorError(AngrExplorationTechniqueError):
+    def __str__(self):
+        return "<OtiegnqwvkDirectorError %s>" % self.message
+
+class AngrTracerError(AngrExplorationTechniqueError):
+    def __str__(self):
+        return "<OtiegnqwvkTracerError %s>" % self.message
+
+#
+# Tracer
+#
+
+class TracerEnvironmentError(AngrError):
+    pass
+
 #
 # Simulation errors
 #
@@ -150,8 +285,13 @@ class SimProcedureArgumentError(SimProcedureError):
 class SimFastPathError(SimEngineError):
     pass
 
-class UnsupportedSyscallError(SimProcedureError, SimUnsupportedError):
+class SimIRSBNoDecodeError(SimIRSBError):
     pass
+
+class AngrUnsupportedSyscallError(AngrSyscallError, SimProcedureError, SimUnsupportedError):
+    pass
+
+UnsupportedSyscallError = AngrUnsupportedSyscallError
 
 class SimReliftException(SimEngineError):
     def __init__(self, state):
