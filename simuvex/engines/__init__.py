4eb7efc5f769540a23e5afde18b46f669db6d41e
mengchen.cmc <mengchen.cmc@alibaba-inc.com>
2017-11-27 08:30:36 +0800

    remove failure, hook

--- simuvex/engines/__init__.py
+++ simuvex/engines/__init__.py
@@ -4,5 +4,3 @@ from .engine import SimEngine
 from .vex import SimEngineVEX
 from .procedure import SimEngineProcedure
 from .unicorn_engine import SimEngineUnicorn
-from .failure import SimEngineFailure
-from .hook import SimEngineHook
